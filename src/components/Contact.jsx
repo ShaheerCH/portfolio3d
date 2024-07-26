2d93a7e05af8b74ea53a78271d15a69a8a4b258f
Muhammad Shaheer <shaheer.chry@gmail.com>
2024-07-27 01:51:50 +0500

    Initial commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Shaheer",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
@@ -114,12 +114,12 @@ const Contact = () => {
             />
           </label>
 
-          <button
+          {/* <button
             type='submit'
             className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
           >
             {loading ? "Sending..." : "Send"}
-          </button>
+          </button> */}
         </form>
       </motion.div>
 
